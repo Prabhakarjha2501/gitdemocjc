8783363571975a73873c368d15eabb116a019365
prabhakar <prabhakarjha2501@gmail.com>
2023-07-17 23:46:23 +0530

    by pjha

--- GiTDemoq/src/Demo.java
+++ GiTDemoq/src/Demo.java
@@ -1,6 +1,12 @@
 
 public class Demo {
+	
+	public void m1() {
+		System.out.println("Hellomw");
+	}
 public static void main(String args[]) {
 	System.out.println("Hello");
+	Demo d=new Demo();
+	d.m1();
 	}
 }
