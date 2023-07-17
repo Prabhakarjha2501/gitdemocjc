3b18bd6a56f59fd080e6494aca696e54cd022881
Prabhakarjha2501 <130430865+Prabhakarjha2501@users.noreply.github.com>
2023-07-18 00:11:25 +0530

    by pjha

--- GiTDemoq/src/Demo.java
+++ GiTDemoq/src/Demo.java
@@ -7,6 +7,9 @@ public class Demo {
 	public void m2() {
 		System.out.println("Hellom222");
 	}
+	public void m3() {
+		System.out.println("Hellom3333333");
+	}
 public static void main(String args[]) {
 	System.out.println("Hello");
 	Demo d=new Demo();
